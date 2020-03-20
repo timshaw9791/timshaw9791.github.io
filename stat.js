34e83213c81f0b2a8d20391c7c834de377a3fdf3
timshaw9791 <84539606@qq.com>
2020-03-20 08:17:10 +0800

    exercise4

---
+++ stat.js
@@ -0,0 +1,29 @@
+(function(){
+
+    function reduce(array, combine, start) {
+        var current = start;//6
+        for (var element of array) {
+            current = combine(current, element);
+        }
+        return current;
+    }   
+    var max=function(data){
+        return reduce(
+            data, 
+            function(a,b){
+                return a>b.credit?a:b.credit;
+                }
+            , 0)
+    }
+
+    window.my={"max":max};
+
+}())
+
+
+   
+
+
+    
+    
+
